3e471a4e7b947d53a7c6fc2a04cfa607a534e2c0
freakdoctor <69394610+freakdoctor@users.noreply.github.com>
2020-08-09 01:59:42 +0200

    Update FIS__Software.ino

--- Fis Color/FIS__Software/FIS__Software.ino
+++ Fis Color/FIS__Software/FIS__Software.ino
@@ -6,7 +6,7 @@
 
 */
 
-// 2" TFT ILI9229 176X220
+// 2" TFT ILI9225 176X220
 
 /*
    _    ___ ___ ___    _   ___ ___ ___ ___
@@ -202,97 +202,3 @@ void SplashScreen() {
   delay(1000);
   tft.clear();
 }
-
-
-
-
-
-
-/*
-  tft.drawRectangle(0, 0, tft.maxX() - 1, tft.maxY() - 1, COLOR_WHITE);
-  tft.setFont(Terminal6x8);
-  tft.drawText(10, 10, "hello!");
-  delay(1000);
-
-  tft.clear();
-  tft.drawText(10, 20, "clear");
-  delay(1000);
-
-  tft.drawText(10, 30, "text small");
-  tft.setBackgroundColor(COLOR_YELLOW);
-  tft.setFont(Terminal12x16);
-  tft.drawText(90, 30, "BIG", COLOR_RED);
-  tft.setBackgroundColor(COLOR_BLACK);
-  tft.setFont(Terminal6x8);
-  delay(1000);
-
-  tft.drawText(10, 40, "setBacklight off");
-  delay(500);
-  tft.setBacklight(LOW);
-  delay(500);
-  tft.setBacklight(HIGH);
-  tft.drawText(10, 50, "setBacklight on");
-  delay(1000);
-
-  tft.drawRectangle(10, 10, 110, 110, COLOR_BLUE);
-  tft.drawText(10, 60, "rectangle");
-  delay(1000);
-
-  tft.fillRectangle(20, 20, 120, 120, COLOR_RED);
-  tft.drawText(10, 70, "solidRectangle");
-  delay(1000);
-
-  tft.drawCircle(80, 80, 50, COLOR_YELLOW);
-  tft.drawText(10, 80, "circle");
-  delay(1000);
-
-  tft.fillCircle(90, 90, 30, COLOR_GREEN);
-  tft.drawText(10, 90, "solidCircle");
-  delay(1000);
-
-  tft.drawLine(0, 0, tft.maxX() - 1, tft.maxY() - 1, COLOR_CYAN);
-  tft.drawText(10, 100, "line");
-  delay(1000);
-
-  for (uint8_t i = 0; i < 127; i++)
-    tft.drawPixel(random(tft.maxX()), random(tft.maxY()), random(0xffff));
-  tft.drawText(10, 110, "point");
-  delay(1000);
-
-  for (uint8_t i = 0; i < 4; i++) {
-    tft.clear();
-    tft.setOrientation(i);
-    tft.drawRectangle(0, 0, tft.maxX() - 1, tft.maxY() - 1, COLOR_WHITE);
-    tft.drawText(10, 10, "setOrientation (" + String("0123").substring(i, i + 1) + ")");
-    tft.drawRectangle(10, 20, 50, 60, COLOR_GREEN);
-    tft.drawCircle(70, 80, 10, COLOR_BLUE);
-    tft.drawLine(30, 40, 70, 80, COLOR_YELLOW);
-    delay(1000);
-  }
-
-  tft.setOrientation(0);
-  tft.clear();
-  tft.drawText(10, 100, "drawing bitmap");
-  delay(1000);
-  tft.clear();
-  tft.setBackgroundColor(COLOR_BLACK);
-  tft.drawBitmap(0, 0, VW, 180, 220, COLOR_WHITE);
-  delay(5000);
-
-  tft.setOrientation(0);
-  tft.clear();
-  tft.setFont(Terminal12x16);
-  tft.setBackgroundColor(COLOR_YELLOW);
-  tft.drawText(10, 40, "bye!", COLOR_RED);
-  tft.setBackgroundColor(COLOR_BLACK);
-  tft.setFont(Terminal6x8);
-  delay(1000);
-
-  tft.drawText(10, 60, "off");
-  delay(1000);
-
-  tft.setBacklight(false);
-  tft.setDisplay(false);
-
-  while (true);
-*/
